2b7ac12df1e7b26ec5531bb6765781d546f387e9
Andrei Skopenko <andrei@skopenko.net>
2023-11-03 12:14:51 +0200

    Add examples

---
+++ src/config_examples.py
@@ -0,0 +1,44 @@
+# examples
+
+queries = [
+    {
+        "keyword": "restaurants in Tallinn",
+        "min_rating": 4.2,
+        "min_reviews": 10,
+        "has_phone": True,
+        "has_website": True,
+        "sort": {
+            "by": "rating",
+            "order": "desc",
+        },
+    }
+]
+
+
+queries = [
+   {
+        "keyword": "cafes in Tallinn",
+        "min_rating": 4.2,
+        "min_reviews": 10,
+        "has_phone": True,
+        "has_website": True,
+        "sort": {
+            "by": "rating",
+            "order": "desc",
+        },
+    },
+]
+
+queries = [
+   {
+        "keyword": "bars in Tallinn",
+        "min_rating": 4.2,
+        "min_reviews": 10,
+        "has_phone": True,
+        "has_website": True,
+        "sort": {
+            "by": "rating",
+            "order": "desc",
+        },
+    },
+]
